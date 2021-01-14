87143d0882aef9cb1a0364958c4a5810a56bdd94
mack-code0 <macaulayhilbert202@gmail.com>
2021-01-13 16:23:59 -0800

    added view

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express()
 const ratesAPI = require("./api/rates")
 
+app.get("/index", (req, res)=>{
+    var form = "<form action='/api/rates' method='GET'>"
+    +"<input type='text' name='base' placeholder='Base' />"
+    +"<input type='text' name='rates' placeholder='Rates' />"
+    +"<input type='submit' />"
+    +"</form>"
+    res.send(form)
+})
+
 app.use("/api", ratesAPI)
 
 const PORT = process.env.PORT || 3000
