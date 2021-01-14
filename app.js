f491bf9376b669b4972869a44686b9585c90d4a3
mack-code0 <macaulayhilbert202@gmail.com>
2021-01-13 16:46:30 -0800

    removed the view

--- app.js
+++ app.js
@@ -2,15 +2,6 @@ const express = require('express');
 const app = express()
 const ratesAPI = require("./api/rates")
 
-app.get("/", (req, res)=>{
-    var form = "<form action='/api/rates' method='GET'>"
-    +"<input type='text' name='base' placeholder='Base' />"
-    +"<input type='text' name='rates' placeholder='Rates' />"
-    +"<input type='submit' />"
-    +"</form>"
-    res.send(form)
-})
-
 app.use("/api", ratesAPI)
 
 const PORT = process.env.PORT || 3000
