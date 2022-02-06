8bd0e483a5fdac99c64d8d28bfea5917ec39b76b
mack-code0 <macaulayhilbert202@gmail.com>
2022-02-06 23:07:46 +0100

    completed test

--- api/rates.js
+++ api/rates.js
@@ -3,10 +3,8 @@ const route = express.Router()
 const axios = require("axios").default
 
 route.get("/api/rates", (req, res)=>{
-    const {base, symbols} = req.query
+    const {symbols} = req.query
 
-    console.log(base+" "+symbols);
-    
     axios({
         method: "GET",
         url: "http://api.exchangeratesapi.io/v1/latest?access_key="+process.env.MY_KEY+"&symbols="+symbols+"&format=1",
@@ -23,7 +21,6 @@ route.get("/api/rates", (req, res)=>{
     }).catch((err)=>{
         res.statusCode = 400
         res.json({
-            "more": base+" "+symbols,
             'error': "An error occured while parsing your request"
         })
     })
